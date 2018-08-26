e4733cca654eb34650f4746d212de2eca68e3c8b
CanRau <cansrau@gmail.com>
2018-08-25 23:22:13 -0500

    feat(aliases): init shared aliases

---
+++ .shellrc/rc.d/aliases.sh
@@ -0,0 +1,10 @@
+alias .=source
+
+alias 'rm=rm'
+
+# -------------------------------------------------------------------
+# Some custom git aliases
+# -------------------------------------------------------------------
+alias git=hub
+alias gpr='hub pull-request'
+alias gai='git add --interactive'
--- .shellrc/zshrc.d/aliases.zsh
+++ .shellrc/zshrc.d/aliases.zsh
@@ -1,10 +1,3 @@
-alias .=source
-
-# -------------------------------------------------------------------
-# make some commands (potentially) less destructive
-# -------------------------------------------------------------------
-alias 'rm=rm'
-
 # list top 10 most frequently used shell commands
 # https://coderwall.com/p/eidi5q/create-a-zsh-function
 alias top10="history | commands | sort -rn | head"
